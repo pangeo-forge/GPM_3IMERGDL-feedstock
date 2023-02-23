2fa672ee6036f5c07e353940f9772774e5cfd248
YuviPanda <yuvipanda@gmail.com>
2023-02-22 20:41:26 -0800

    Simplify use of pangeo_forge_cmr

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -3,30 +3,24 @@ A recipe to move GPM_#IMERGDL from <DC> to a cloud analysis ready format.
 """
 
 import apache_beam as beam
-from pangeo_forge_cmr import get_cmr_granule_links
+from pangeo_forge_cmr import files_from_cmr
 from pangeo_forge_earthdatalogin import OpenURLWithEarthDataLogin
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
-from pangeo_forge_recipes.transforms import (OpenURLWithFSSpec, OpenWithXarray,
-                                             StoreToZarr)
+from pangeo_forge_recipes.transforms import OpenWithXarray, StoreToZarr
 
-shortname = 'GPM_3IMERGDL'
-
-all_files = get_cmr_granule_links(shortname)
-
-pattern = pattern_from_file_sequence(
-    all_files,
+files = files_from_cmr(
+    shortname='GPM_3IMERGDL',
     concat_dim="time",
-    nitems_per_file=1,
+    nitems_per_file=1
 )
 
 recipe = (
-    beam.Create(pattern.items())
+    beam.Create(files.items())
     | OpenURLWithEarthDataLogin()
     | OpenWithXarray()
     | StoreToZarr(
         store_name='gpm-3imergdl',
         target_chunks={'time': 2},
-        combine_dims=pattern.combine_dim_keys,
+        combine_dims=files.combine_dim_keys,
     )
 )
 
